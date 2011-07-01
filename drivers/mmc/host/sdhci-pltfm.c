0bec0fb2915ca32a6d230ab358bc7e2545fda89e
Shawn Guo <shawn.guo@linaro.org>
2011-07-01 09:35:28 +0800

    mmc: sdhci-pltfm: dt device does not pass parent to sdhci_alloc_host
    Neither platform based nor dt based device needs to pass the parent
    to sdhci_alloc_host.  There is no difference between platform and dt
    on this point.
    
    The patch makes the change to pass device itself than its parent to
    sdhci_alloc_host for dt case too.  Otherwise the probe function of
    sdhci based drivers which is shared between platform and dt will
    fail on dt case.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Cc: Chris Ball <cjb@laptop.org>
    Acked-by: Grant Likely <grant.likely@secretlab.ca>
    Acked-by: Chris Ball <cjb@laptop.org>
    [Xulin:Original patch taken from
    Freescale-Linux-SDK-for-LS1021A-IOT-Rev2-v0.4-SOURCE-20150907-yocto.iso]
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- drivers/mmc/host/sdhci-pltfm.c
+++ drivers/mmc/host/sdhci-pltfm.c
@@ -143,6 +143,7 @@ struct sdhci_host *sdhci_pltfm_init(struct platform_device *pdev,
 {
 	struct sdhci_host *host;
 	struct sdhci_pltfm_host *pltfm_host;
+	struct device_node *np = pdev->dev.of_node;
 	struct resource *iomem;
 	int ret;
 
@@ -155,6 +156,11 @@ struct sdhci_host *sdhci_pltfm_init(struct platform_device *pdev,
 	if (resource_size(iomem) < 0x100)
 		dev_err(&pdev->dev, "Invalid iomem size!\n");
 
+	/* Some PCI-based MFD need the parent here */
+	if (pdev->dev.parent != &platform_bus && !np)
+		host = sdhci_alloc_host(pdev->dev.parent,
+			sizeof(struct sdhci_pltfm_host) + priv_size);
+	else
 	host = sdhci_alloc_host(&pdev->dev,
 		sizeof(struct sdhci_pltfm_host) + priv_size);
 
