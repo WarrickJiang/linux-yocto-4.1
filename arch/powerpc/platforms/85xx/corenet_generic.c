81256411e1ebba83c7752b040e7281e1cc652be2
Tiejun Chen <tiejun.chen@windriver.com>
2012-12-05 17:32:13 +0800

    fman: introduce fman_init_early function
    We can't reset B/QMAN in dump kernel. So we just free all
    the B/Qman resources before booting the dump kernel. Since we
    also need to shutdown FMAN before we free these resources,
    introduce fman_init_early function for kexec to register
    a crash shutdown hooks to do this.
    
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>
    [Modify for current kernel context.]
    Signed-off-by: Tiejun Chen <tiejun.chen@windriver.com>
    Signed-off-by: Bin Jiang <bin.jiang@windriver.com>

--- arch/powerpc/platforms/85xx/corenet_generic.c
+++ arch/powerpc/platforms/85xx/corenet_generic.c
@@ -206,6 +206,9 @@ static int __init corenet_generic_probe(void)
 /* Early setup is required for large chunks of contiguous (and coarsely-aligned)
  * memory. The following shoe-horns Q/Bman "init_early" calls into the
  * platform setup to let them parse their CCSR nodes early on. */
+#ifdef CONFIG_FSL_FMAN
+void __init fman_init_early(void);
+#endif
 #ifdef CONFIG_FSL_QMAN_CONFIG
 void __init qman_init_early(void);
 #endif
@@ -215,6 +218,9 @@ void __init bman_init_early(void);
 
 static __init void corenet_ds_init_early(void)
 {
+#ifdef CONFIG_FSL_FMAN
+	fman_init_early();
+#endif
 #ifdef CONFIG_FSL_QMAN_CONFIG
 	qman_init_early();
 #endif
