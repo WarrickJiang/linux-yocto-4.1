fe54f322f0c2ff65d6a2342f270ab44d3ad9e60e
Lu.Jiang <lu.jiang@windriver.com>
2014-09-11 17:15:24 +0800

    dpaa_eth: move external declaration to header
    [Original patch taken from QorIQ-SDK-V1.6-SOURCE-20140619-yocto.iso]
    
    Moved qman_portal_max external declaration to header file to avoid
    checkpatch warning.
    
    Signed-off-by: Madalin Bucur <madalin.bucur@freescale.com>
    Change-Id: I67b6fef624033db3d0de9c75c883d72af07bc264
    Reviewed-on: http://git.am.freescale.net:8181/7452
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Cristian-Constantin Sovaiala <Cristian.Sovaiala@freescale.com>
    Reviewed-by: Marian Cristian Rotariu <marian.rotariu@freescale.com>
    Reviewed-on: http://git.am.freescale.net:8181/7701
    Signed-off-by: Lu.Jiang <lu.jiang@windriver.com>

--- drivers/net/ethernet/freescale/dpa/dpaa_eth_common.h
+++ drivers/net/ethernet/freescale/dpa/dpaa_eth_common.h
@@ -94,6 +94,9 @@ typedef enum dpaa_eth_hook_result (*dpaa_eth_egress_hook_t)(
 typedef enum dpaa_eth_hook_result (*dpaa_eth_confirm_hook_t)(
 		struct net_device *net_dev, const struct qm_fd *fd, u32 fqid);
 
+/* used in napi related functions */
+extern u16 qman_portal_max;
+
 /* Various hooks used for unit-testing and/or fastpath optimizations.
  * Currently only one set of such hooks is supported.
  */
