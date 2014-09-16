5c75314176a31d4ef17bd8bf73094c04fd14340b
Lu.Jiang <lu.jiang@windriver.com>
2014-09-16 14:17:33 +0800

    powerpc/booke64: Add LRAT next and max entries to tlb_per_core structure
    [Original patch taken from QorIQ-SDK-V1.6-SOURCE-20140619-yocto.iso]
    
    LRAT (Logical to Real Address Translation) is shared between hw threads.
    Add LRAT next and max entries to tlb_per_core data structure and initialize them.
    
    Signed-off-by: Mihai Caraman <mihai.caraman@freescale.com>
    (cherry picked and merged from sdk1.5 commit aa10cefcc1588338b3a80e842bc4db351dcec176)
    Change-Id: If229acd9f4ae28fb49ce42132831d720bd436b84
    Reviewed-on: http://git.am.freescale.net:8181/11558
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Stuart Yoder <stuart.yoder@freescale.com>
    Reviewed-by: Jose Rivera <German.Rivera@freescale.com>
    Signed-off-by: Lu.Jiang <lu.jiang@windriver.com>

--- arch/powerpc/include/asm/mmu-book3e.h
+++ arch/powerpc/include/asm/mmu-book3e.h
@@ -220,6 +220,12 @@
 #define TLBILX_T_CLASS2			6
 #define TLBILX_T_CLASS3			7
 
+/* LRATCFG bits */
+#define LRATCFG_ASSOC		0xFF000000
+#define LRATCFG_LASIZE		0x00FE0000
+#define LRATCFG_LPID		0x00002000
+#define LRATCFG_NENTRY		0x00000FFF
+
 #ifndef __ASSEMBLY__
 #include <asm/bug.h>
 
@@ -297,6 +303,7 @@ struct tlb_core_data {
 
 	/* For software way selection, as on Freescale TLB1 */
 	u8 esel_next, esel_max, esel_first;
+	u8 lrat_next, lrat_max;
 };
 
 #ifdef CONFIG_PPC64
